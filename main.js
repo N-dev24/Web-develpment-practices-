c48819651b8db8fc1a29a4d78f78565a75b1951f
N-dev24 <171658067+N-dev24@users.noreply.github.com>
2026-03-09 00:09:24 -0700

    feat: implement dynamic project rendering via GitHub API
    - Fetch folder contents asynchronously
    - Filter directories to generate project cards
    - Add error handling for empty or missing sections

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 const githubUser = 'N-dev24';
 const githubRepo = 'Web-develpment-practices-'
+
+async function cargarCarpeta (nombreCarpeta, idContenedorHTML) {
+    const contenedor = document.getElementById(idContenedorHTML);
+    
+    try {
+        const respuesta = await fetch (`https://api.github.com/repos/${githubUser}/${githubRepo}/contents/${nombreCarpeta}`);
+
+        if (!respuesta.ok) {
+            contenedor.innerHTML = `<p style="color: gray;">Aún no hay proyectos en la sección ${nombreCarpeta}.</p>`;
+            return;
+        }
+
+        const archivos = await respuesta.json();
+        contenedor.innerHTML = '';
+        
+        archivos.forEach(item => {
+            if (item.type === 'dir') {
+                const tarjetaHTML = ` 
+                <article class="card">
+                   <h3>${idContenedorHTML.name.replace(/-/g, ' ')}
+                   <a href="./${nombreCarpeta}/${item.name}/index.html" class="btn">Ver proyecto</a>
+                </article>
+            `;
+            contenedor.innerHTML += tarjetaHTML;
+            }
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos", error);
+    }
+}
