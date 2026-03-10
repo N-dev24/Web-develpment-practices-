eb6277b16376b6648a5bff3d234c9fc4ae2e3d57
ayato <ayatouwo09@gmail.com>
2026-03-10 14:08:51 -0700

    refactor(db): restructure connection to include product model

--- Clase/SISWEB-BACKEND/src/connection/connection.ts
+++ Clase/SISWEB-BACKEND/src/connection/connection.ts
@@ -1,24 +1,26 @@
 
-import { Sequelize } from "sequelize-typescript";  
+
+import { Sequelize } from "sequelize-typescript"; 
+import { Product } from "../models/product"; 
 
 const connection = new Sequelize({ 
-  database: 'sisweb_db', 
-  dialect: 'postgres',
-  username: 'sisweb_user', 
-  password: 'HDK#$%Ljkwerff.89', 
-  storage: ':memory:', 
-  models: [ ] 
-}); 
+database: 'sisweb_db', 
+dialect: 'postgres', 
+username: 'sisweb_user', 
+password: 'HDK#$%Ljkwerff.89', 
+storage: ':memory:', 
+models: [ 
+Product 
+] 
+});
 
-async function connectionDB() {
-  try {
-    await connection.authenticate(); // authenticate verifica la conexión
-    console.log("Conexión exitosa a la base de datos MySQL.");
-    await connection.sync();
-  } catch (e) {
-    console.log("Error al conectar con la base de datos:", e);
-  }
-}
 
-export default connectionDB;
-    
+
+async function connectionDB(){ 
+try{ 
+await connection.sync(); 
+}catch(e){ 
+console.log(e); 
+} 
+} 
+export default connectionDB; 
