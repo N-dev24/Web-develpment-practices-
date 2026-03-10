10121110155cf87c6c731fda9432afad353bbdda
ayato <ayatouwo09@gmail.com>
2026-03-10 15:08:07 -0700

    fix(controllers): resolve TypeScript typing and duplicate errors in product controller

--- Clase/SISWEB-BACKEND/src/controler/productControler.ts
+++ Clase/SISWEB-BACKEND/src/controler/productControler.ts
@@ -1,41 +1,8 @@
 import { RequestHandler, Request, Response } from "express";
 import { Product } from "../models/product"; 
- 
- 
-// Create and Save a new Product 
-export const createProduct: RequestHandler = RequestHandler = (req: Request, res: Response) => { 
- 
- 
-   
-} 
- 
-// Retrieve all Products from the database. 
-export const getAllProducts: RequestHandler = (req: Request, res: Response) => { 
- 
-  
-}; 
- 
-// Find a single Product with an id 
-export const getProductById: RequestHandler = (req: Request, res: Response) => { 
- 
-   
-}; 
- 
-// Update a Product by the id in the request 
-export const modifyProduct: RequestHandler = async (req: Request,res: Response) => { 
- 
-  
-}; 
- 
-// Delete a Product with the specified id in the request 
-export const deleteProduct: RequestHandler = async (req: Request,res: Response) => { 
- 
-  
-}; 
 
-
-//Create new product 
-export const createProduct: RequestHandler = (req: Request, res: Response) => { 
+// Create new product 
+export const createProduct: RequestHandler = (req: Request, res: Response): any => { 
   //Validate request 
   if (!req.body) { 
     return res.status(400).json({ 
@@ -45,7 +12,7 @@ export const createProduct: RequestHandler = (req: Request, res: Response) => {
     }); 
   } 
    
-// Save Product in the database 
+  // Save Product in the database 
   const product = { ...req.body }; 
   Product.create(product) 
     .then((data: Product | null) => { 
@@ -55,7 +22,7 @@ export const createProduct: RequestHandler = (req: Request, res: Response) => {
         payload: data, 
       }); 
     }) 
-    .catch((err) => { 
+    .catch((err: any) => { 
        res.status(500).json({ 
          status: "error", 
          message: "Something happened creating a product. " + err.message, 
@@ -65,9 +32,8 @@ export const createProduct: RequestHandler = (req: Request, res: Response) => {
 }; 
 
 // Get all products using Promises
-export const getAllProducts: RequestHandler = (req: Request, res: Response) => { 
+export const getAllProducts: RequestHandler = (req: Request, res: Response): any => { 
   //Calling the Sequelize findAll method. This is the same that a SELECT * FROM PRODUCT in a SQL query. 
-   
    Product.findAll() 
    .then((data: Product[]) => { 
       return res.status(200).json({ 
@@ -76,7 +42,7 @@ export const getAllProducts: RequestHandler = (req: Request, res: Response) => {
            payload: data, 
       }); 
     }) 
-    .catch((err) => { 
+    .catch((err: any) => { 
        return res.status(500).json({ 
        status: "error", 
        message: "Something happened retrieving all products. " + err.message, 
@@ -85,11 +51,9 @@ export const getAllProducts: RequestHandler = (req: Request, res: Response) => {
   }); 
 }; 
 
-
-
 /// Get products by Id 
-export const getProductById: RequestHandler = (req: Request, res: Response) => { 
-  Product.findByPk(req.params.id) 
+export const getProductById: RequestHandler = (req: Request, res: Response): any => { 
+  Product.findByPk(req.params.id as any) 
   .then((data: Product | null) => { 
     return res.status(200).json({ 
       status: "success", 
@@ -97,7 +61,7 @@ export const getProductById: RequestHandler = (req: Request, res: Response) => {
       payload: data, 
     }); 
   }) 
-  .catch((err) => { 
+  .catch((err: any) => { 
     return res.status(500).json({ 
       status: "error", 
       message: "Something happened retrieving all products. " + err.message, 
@@ -106,10 +70,8 @@ export const getProductById: RequestHandler = (req: Request, res: Response) => {
   }); 
 }; 
 
-
-
 ///Modify product 
-export const modifyProduct:RequestHandler = (req: Request, res: Response) => { 
+export const modifyProduct: RequestHandler = (req: Request, res: Response): any => { 
   // Validate request 
   if (!req.body) { 
     return res.status(400).json({ 
@@ -119,7 +81,7 @@ export const modifyProduct:RequestHandler = (req: Request, res: Response) => {
     }); 
   } 
 
-// Save Product in the database 
+  // Save Product in the database 
   Product.update({ ...req.body }, { where: { id: req.params.id } }) 
   .then((isUpdated) => { 
     if (isUpdated) { 
@@ -136,7 +98,7 @@ export const modifyProduct:RequestHandler = (req: Request, res: Response) => {
     }); 
    } 
   }) 
-  .catch((err) => { 
+  .catch((err: any) => { 
     res.status(500).json({ 
       status: "error", 
       message: "Something happened updating a product. " + err.message, 
@@ -145,46 +107,16 @@ export const modifyProduct:RequestHandler = (req: Request, res: Response) => {
 }); 
 }; 
 
-
-
 ///Delete product
-export const deleteProduct: RequestHandler = async (req: Request, res: Response): Promise<void> => { 
+export const deleteProduct: RequestHandler = async (req: Request, res: Response): Promise<any> => { 
     const { id } = req.body; 
     try { 
       await Product.destroy({ where: { id } }); 
       res.status(200).json({ message: "Product deleted" }); 
-    } catch (error) { 
+    } catch (error: any) { 
       res.status(500).json({ 
         message: "Error deleting products", 
         error, 
       }); 
     } 
 }; 
-
-
-
-// Get all products 
-export const getAllProducts: RequestHandler = async (req:Request, res:Response)=>{ 
-try { 
-const products:Array<Product> = await Product.findAll(); 
-return res.status(200).json(products); 
-} catch (error) { 
-return res.status(500).json({ 
-"message":"Error getting products",  
-error 
-}); 
-} 
-} 
-
-// Get product by Id 
-export const getProductById: RequestHandler = async (req:Request, res:Response)=>{ 
-try { 
-const product:Product = await Product.findByPk(req.params.id); 
-return res.status(200).json(product); 
-} catch (error) { 
-return res.status(500).json({ 
-"message":"Error getting products",  
-error 
-}); 
-} 
-}
