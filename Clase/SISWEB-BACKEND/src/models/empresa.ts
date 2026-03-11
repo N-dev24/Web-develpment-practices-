eae2e0d1458e3855a681c9f59f42cf78f8dfb8f9
ayato <ayatouwo09@gmail.com>
2026-03-10 18:20:15 -0700

    feat(models): create Empresa model

---
+++ Clase/SISWEB-BACKEND/src/models/empresa.ts
@@ -0,0 +1,34 @@
+import { Table, Model, Column, CreatedAt, UpdatedAt, DataType } from 'sequelize-typescript'; 
+import { Optional } from 'sequelize'; 
+
+interface EmpresaAttributes { 
+  id: number; 
+  nombre: string; 
+  contacto: string; 
+  ubicacion: string; 
+} 
+
+interface EmpresaCreationAttributes extends Optional<EmpresaAttributes, 'id'> {} 
+
+@Table({ 
+  tableName: "Empresas" 
+}) 
+export class Empresa extends Model<EmpresaAttributes, EmpresaCreationAttributes> { 
+
+  @Column 
+  nombre!: string; 
+
+  @Column 
+  contacto!: string; 
+
+  @Column 
+  ubicacion!: string; 
+
+  @CreatedAt 
+  @Column 
+  createdAt!: Date; 
+
+  @UpdatedAt 
+  @Column 
+  updatedAt!: Date; 
+}
