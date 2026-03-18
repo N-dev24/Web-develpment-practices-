e0635d89b379f081ff7ebc99a86e10abb82c04bd
N-dev24 <171658067+N-dev24@users.noreply.github.com>
2026-03-17 23:46:25 -0700

    feat: add eventlistener to boton const

--- Personal/Todolist/main.js
+++ Personal/Todolist/main.js
@@ -2,3 +2,24 @@ const input = document.getElementById('TareaInput');
 const boton = document.getElementById('tareaBtn');
 const lista = document.getElementById('listaTareas');
 
+boton.addEventListener('click', () => {
+
+    //validamos que el input no este vacio
+     if (input.value === "") return;
+
+     //creamos un nuevo elemento de la lista
+     const nuevaTarea = document.createElement('li');
+     nuevaTarea.innerText = input.value;
+
+     //agregamos un evento para que se borre al hacer click
+     nuevaTarea.addEventListener('click', () => {
+        nuevaTarea.remove();
+     })
+
+     //metemos la tarea dentro de la lista
+     lista.appendChild(nuevaTarea);
+
+     //limpiamos el input para la siguiente tarea
+     input.value = "";
+
+})
