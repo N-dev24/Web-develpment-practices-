ba8a36a07856ef705bd76272a1b954722f651ac1
ayato <ayatouwo09@gmail.com>
2026-03-27 19:00:34 -0700

    build: add cors

--- Clase/SISWEB-BACKEND/index.ts
+++ Clase/SISWEB-BACKEND/index.ts
@@ -3,12 +3,13 @@ import express, { Express } from "express";
 import apiRouter from './src/routes'; 
 import connectionDB from './src/connection/connection'; 
 const morgan = require('morgan'); 
+const cors = require('cors') 
 
 
 const app: Express = express(); 
 const port = 3006; 
 
-
+app.use(cors({ origin: true })); 
 app.use(morgan('dev')); 
 app.use(express.json());  
 app.use(apiRouter); 
