11864d09bfcfbae0fc3453d0d6ce1c7c5c5aa7ff
ayato <ayatouwo09@gmail.com>
2026-04-01 13:09:57 -0700

    fix: add corret route for Products in routes index

--- Clase/SISWEB-BACKEND/src/routes/index.ts
+++ Clase/SISWEB-BACKEND/src/routes/index.ts
@@ -4,7 +4,7 @@ import productRoutes from './productRoutes';
 
 const apiRouter:Router = Router();  
 
-apiRouter.use('/products', productRoutes);
+apiRouter.use('/product', productRoutes);
 
 apiRouter.get('/', (req:Request, res: Response) => {  
 res.send('Hello World!')  
